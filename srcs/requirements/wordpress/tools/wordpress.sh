6e6c9b7e3cb9859ebb294c29982803ba1e5f4ff6
moojig12 <moojig5152@gmail.com>
2025-07-22 12:14:11 +0200

    updated wordpress

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
-cd /var/www/html
+
+mkdir -p /var/www/wordpress
+
+cd /var/www/wordpress
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
 
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=$db1_name --dbuser=$db1_user --dbpass=$db1_pass --dbhost=$db_host --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+
+sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${db1_name}' );/" wp-config.php
+sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${db1_user}' );/" wp-config.php
+sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${db1_pass}' );/" wp-config.php
+sed -i "s/localhost/mariadb/" wp-config.php
+
+sleep 10
+
+wp core install \
+  --url="${DOMAIN_NAME}" \
+  --title="${SITE_TITLE}" \
+  --admin_user="${WP_ADMIN_USER}" \
+  --admin_password="${WP_ADMIN_PASSWORD}" \
+  --admin_email="${WP_ADMIN_EMAIL}" \
+  --allow-root
+
+wp user create \
+    "${WP_USER}" "${WP_USER_EMAIL}" \
+    --user_pass="${WP_USER_PASSWORD}" \
+    --role=editor \
+    --allow-root
+
+mkdir -p /run/php
 
 php-fpm7.4 -F
