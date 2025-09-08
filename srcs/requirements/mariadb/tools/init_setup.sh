88198a89a2fa1eba50f47ce5551b8b37aefa216c
moojig12 <moojig5152@gmail.com>
2025-09-08 11:35:41 +0200

    changes

--- srcs/requirements/mariadb/tools/init_setup.sh
+++ srcs/requirements/mariadb/tools/init_setup.sh
@@ -1,5 +1,7 @@
+DB_PASS="$(cat /run/secrets/db_pass.txt)";
+
 echo "CREATE DATABASE IF NOT EXISTS ${db1_name} ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '${db1_user}'@'%' IDENTIFIED BY '${db1_pwd}' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS '${db1_user}'@'%' IDENTIFIED BY '${DB_PASS}' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON ${db1_name}.* TO '${db1_user}'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
